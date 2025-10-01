837353800f31a025c4e42a33cabc02b6672f69d8
weijing-wang <149795312+weijing-wang@users.noreply.github.com>
2025-10-01 12:34:33 +0000

    Deploying to gh-pages from @ weijing-wang/weijing-wang.github.io@0254c7d47f9c82dc3489481eaea951165b2cd38d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-cv",
+  },{id: "nav-cv",
           title: "cv",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
+          },
+        },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
           },
         },{id: "nav-projects",
           title: "projects",
