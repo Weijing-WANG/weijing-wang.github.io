ec41c37e3977cd65a225f14776f64ee10d178b25
weijing-wang <149795312+weijing-wang@users.noreply.github.com>
2025-10-02 15:28:58 +0000

    Deploying to gh-pages from @ weijing-wang/weijing-wang.github.io@f928a2bf54138f0dcf554fe05c551162444d54a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -380,9 +380,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-third-places-amp-urban-vitality",
+          title: 'Third Places &amp;amp; Urban Vitality',
+          description: "Investigating the causal relationship between third places and urban vitality in Singapore",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
