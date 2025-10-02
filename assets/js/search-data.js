5176143102e00f751784c53a3328f360be2d0ffd
weijing-wang <149795312+weijing-wang@users.noreply.github.com>
2025-10-02 11:16:44 +0000

    Deploying to gh-pages from @ weijing-wang/weijing-wang.github.io@4cfe4aa2817e78e85b56dc2085a0915e37a40141 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Research projects and collaborations in urban analytics and computational social science",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
