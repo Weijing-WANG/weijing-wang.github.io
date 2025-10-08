e5a59df68fec4ef35c6ec4d927237f77f70996ac
weijing-wang <149795312+weijing-wang@users.noreply.github.com>
2025-10-08 14:11:04 +0000

    Deploying to gh-pages from @ weijing-wang/weijing-wang.github.io@f370fd241f95154f34a4fa0f1f29aa59761c6304 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -369,16 +369,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-new-publication-in-land-journal-our-research-uses-street-view-imagery-and-remote-sensing-to-analyze-spatial-drivers-of-urban-industrial-agglomeration-in-shanghai-read-paper",
-          title: 'New publication in Land journal! Our research uses street view imagery and remote...',
+            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "news-",
-          title: '',
+          section: "News",},{id: "news-new-paper-published-in-caad-futures-2025",
+          title: 'New Paper Published in CAAD Futures 2025',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-new-publication-in-land-journal-our-research-uses-street-view-imagery-and-remote-sensing-to-analyze-spatial-drivers-of-urban-industrial-agglomeration-in-shanghai-read-paper",
+          title: 'New publication in Land journal! Our research uses street view imagery and remote...',
           description: "",
           section: "News",},{id: "projects-third-places-amp-urban-vitality",
           title: 'Third Places &amp;amp; Urban Vitality',
